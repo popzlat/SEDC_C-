bc36777360200574b8eb5373be41d12d15668e1f
popzlat <zlatko.p4@gmail.com>
2019-03-03 16:59:04 +0100

    task 01 changes

--- Homework01/Task01/realCalculator.cs
+++ Homework01/Task01/realCalculator.cs
@@ -12,37 +12,41 @@ namespace Task01
         {
             Console.WriteLine("Enter the first number");
             int firstNumber = int.Parse(Console.ReadLine());
+
+
             Console.WriteLine("Enter the second number");
             int secondNumber = int.Parse(Console.ReadLine());
+          
             Console.WriteLine("What operation would like to be done (+, - , * , /)");
             string operaTor = Console.ReadLine();
             int result = 0;
-            
-            if(operaTor != "+" && operaTor != "-" && operaTor != "*" && operaTor != "/"){
-            switch(operaTor){
-                case "+":
+         
+            if (operaTor != "+" && operaTor != "-"){
+                Console.WriteLine("You enter wrong operator");
+}
+            else{
+                switch (operaTor){
+                   case "+":
                     result = firstNumber + secondNumber;
+                        Console.WriteLine("Result is " + result);
                     break;
                     case "-": 
                     result = firstNumber - secondNumber;
+                        Console.WriteLine("Result is " + result);
                     break;
                     case "*":
                     result = firstNumber * secondNumber;
+                        Console.WriteLine("Result is " + result);
                     break;
                     case "/":
                     result = firstNumber / secondNumber;
+                        Console.WriteLine("Result is " + result);
                     break;
 
                     default:
                     Console.WriteLine("Please enter the right operator ");
-                    break;
-
-                    }
-                }
-            else{
-                Console.WriteLine("Something went wrong try again");
-}               
-
+                    break;	}
+}
 
             Console.ReadLine();
 
